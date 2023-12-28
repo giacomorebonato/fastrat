f08c560f6bcc4a7fa662ae2de734a64c165e7b96
Giacomo Rebonato <grebonato@outlook.ie>
2023-12-28 16:25:48 +0100

    fixing favicon

--- src/features/browser/head.tsx
+++ src/features/browser/head.tsx
@@ -1,8 +1,10 @@
+import favicon from './favicon.ico'
+
 export function Head() {
 	return (
 		<>
 			<meta name='viewport' content='width=device-width, initial-scale=1.0' />
-			<link rel='icon' type='image/x-icon' href='/public/images/favicon.ico' />
+			<link rel='icon' type='image/x-icon' href={favicon} />
 		</>
 	)
 }
