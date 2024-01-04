7e407e16415072c43dcf491da696b88edca0fc9c
Giacomo Rebonato <grebonato@outlook.ie>
2024-01-04 15:35:59 +0000

    changed to handler syntax

---
+++ src/handler.ts
@@ -0,0 +1,55 @@
+import type { IncomingMessage, ServerResponse } from 'node:http'
+import type { Socket } from 'node:net'
+import { createServer } from '#features/server/create-server'
+
+const server = await createServer()
+
+declare module 'node:net' {
+	interface Socket {
+		server: import('node:http').Server
+	}
+}
+
+declare module 'node:http' {
+	interface Server {
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		vaviteUpgradeHandler?: any
+	}
+}
+
+declare global {
+	// biome-ignore lint/style/noVar: <explanation>
+	var ready: boolean
+}
+
+globalThis.ready = false
+
+export default async function handler(
+	request: IncomingMessage,
+	reply: ServerResponse,
+) {
+	if (!ready) {
+		await server.ready()
+
+		const socketServer = request.socket.server
+		if (socketServer.vaviteUpgradeHandler) {
+			socketServer.off('upgrade', socketServer.vaviteUpgradeHandler)
+		}
+
+		socketServer.on('upgrade', handleUpgrade)
+		socketServer.vaviteUpgradeHandler = handleUpgrade
+
+		ready = true
+	}
+
+	server.server.emit('request', request, reply)
+}
+
+function handleUpgrade(request: IncomingMessage, socket: Socket, head: Buffer) {
+	if (request.url === '/') {
+		// Don't clash with vite's websocket server
+		return
+	}
+
+	server.server.emit('upgrade', request, socket, head)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,9 @@
-import httpDevServer from 'vavite/http-dev-server'
 import { createServer } from '#features/server/create-server'
 import { env } from '#features/server/env.js'
 
-declare global {
-	// biome-ignore lint/style/noVar: <explanation>
-	var server: Awaited<ReturnType<typeof createServer>>
-}
+const server = await createServer()
 
-globalThis.server = await createServer()
-
-if (httpDevServer) {
-	httpDevServer.on('request', (request, reply) => {
-		server.server.emit('request', request, reply)
-	})
-
-	httpDevServer.on('upgrade', (request, socket, head) => {
-		if (request?.url === '/trpc') {
-			server.server.emit('upgrade', request, socket, head)
-		}
-	})
-
-	httpDevServer.on('close', async () => {
-		await server.close()
-	})
-} else {
-	server.listen({
-		host: env.HOST,
-		port: env.PORT,
-	})
-}
+server.listen({
+	host: env.HOST,
+	port: env.PORT,
+})
