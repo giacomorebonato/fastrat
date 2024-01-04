53bef2e724ae5274e37eec4dcca5dd7fb003ce2e
Giacomo Rebonato <grebonato@outlook.ie>
2024-01-04 16:06:44 +0000

    fix tests

--- src/features/server/env.ts
+++ src/features/server/env.ts
@@ -4,8 +4,8 @@ import { z } from 'zod'
 
 const schema = z.object({
 	CI: z.optional(z.string().transform(Boolean)),
-	GOOGLE_CLIENT_ID: z.optional(z.string()),
-	GOOGLE_CLIENT_SECRET: z.optional(z.string()),
+	GOOGLE_CLIENT_ID: z.string().default('fake-client-id'),
+	GOOGLE_CLIENT_SECRET: z.string().default('fake-client-secret'),
 	HOST: z.string().default('0.0.0.0'),
 	NODE_ENV: z
 		.enum(['development', 'test', 'production'])
@@ -13,8 +13,7 @@ const schema = z.object({
 	PORT: z.string().transform(Number).default('3000'),
 	SECRET: z.string().default('a-good-repository-secret'),
 	SITE_URL: z.string().default('http://localhost:3000'),
-	TEST_EMAIL: z.optional(z.string()),
-	TEST_PASSWORD: z.optional(z.string()),
+	TEST_EMAIL: z.string().default('fastrat@email.com'),
 	TURSO_DB_AUTH_TOKEN: z.optional(z.string()),
 	TURSO_DB_URL: z.string().default('file:local.db'),
 })
