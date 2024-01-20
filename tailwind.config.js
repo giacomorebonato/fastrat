9cf6560a026e83d21ea9d7a97cc7862a0a57ca38
Giacomo Rebonato <grebonato@outlook.ie>
2024-01-20 11:14:01 +0100

    padding and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 const config = {
 	content: ['./src/**/*.{ts,tsx}'],
 	theme: {
+		fontFamily: {
+      'open-sans': ["OpenSans", "sans-serif"]
+    },
 		extend: {},
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
