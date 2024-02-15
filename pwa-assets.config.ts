a61fb99fd4f192880bdff3f2584660db21c76496
Giacomo Rebonato <grebonato@outlook.ie>
2024-02-15 14:15:16 +0100

    fix pwa

---
+++ pwa-assets.config.ts
@@ -0,0 +1,23 @@
+import {
+	combinePresetAndAppleSplashScreens,
+	defineConfig,
+	minimal2023Preset,
+} from '@vite-pwa/assets-generator/config'
+
+export default defineConfig({
+	headLinkOptions: {
+		preset: '2023',
+		basePath: '/src/images/',
+	},
+	preset: combinePresetAndAppleSplashScreens(
+		{ ...minimal2023Preset },
+		{
+			// dark splash screens using black background (the default)
+			darkResizeOptions: { background: 'black', fit: 'contain' },
+			// or using a custom background color
+			// darkResizeOptions: { background: '#1f1f1f' },
+		},
+		['iPad Air 9.7"'],
+	),
+	images: ['src/images/logo.svg'],
+})
