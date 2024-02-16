91444895805bc1adfd24a1c8a7290f79c085bca9
Giacomo Rebonato <grebonato@outlook.ie>
2024-02-16 15:14:40 +0100

    avoid layout shift

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ const config = {
 	plugins: {
 		autoprefixer: {},
 		tailwindcss: {},
+		// https://tailwindcss.com/docs/optimizing-for-production
+		...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
 	},
 }
 
