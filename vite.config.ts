58ef8903fb7535416cbb4730e6b13eb70a316ecc
Giacomo Rebonato <grebonato@outlook.ie>
2024-02-27 15:53:38 +0100

    adds webp to sw cache

--- vite.config.ts
+++ vite.config.ts
@@ -73,7 +73,7 @@ const config: UserConfig = {
 			registerType: 'prompt',
 			workbox: {
 				maximumFileSizeToCacheInBytes: 10_000_000,
-				globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
 				navigateFallbackDenylist: [/^\/login\/google/],
 				navigateFallback: '/index.html',
 			},
