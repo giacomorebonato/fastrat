c3fbbb0a51e396e4911f1f379efa0cd08ad9594b
Giacomo Rebonato <grebonato@outlook.ie>
2024-02-28 08:44:40 +0100

    fix pwa error

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom/client'
 import { StartClient } from '@tanstack/react-router-server/client'
 import React from 'react'
 import { createRouter } from './browser/create-router'
+import { RouterProvider } from '@tanstack/react-router'
 
 const router = createRouter()
-router.hydrate()
+const divRoot = document.getElementById('root') as HTMLDivElement
 
-ReactDOM.hydrateRoot(
-	// biome-ignore lint/style/noNonNullAssertion: <explanation>
-	document.getElementById('root')!,
-	<React.StrictMode>
-		<StartClient router={router} />
-	</React.StrictMode>,
-)
+// innerHTML contains just the placeholder when nothing has been server rendered
+// it happens in PWA mode
+if (divRoot.innerHTML === '<!--app-html-->') {
+	const root = ReactDOM.createRoot(divRoot)
+	root.render(
+		<React.StrictMode>
+			<RouterProvider router={router} />
+		</React.StrictMode>,
+	)
+} else {
+	router.hydrate()
+
+	ReactDOM.hydrateRoot(
+		divRoot,
+		<React.StrictMode>
+			<StartClient router={router} />
+		</React.StrictMode>,
+	)
+}
